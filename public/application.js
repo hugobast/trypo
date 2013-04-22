2f0482d8261dfa1202e651c3a51ba0c2b67d4dcb
Hugo Bastien <hugobast@gmail.com>
2013-04-21 23:02:52 -0400

    First working version

--- public/application.js
+++ public/application.js
@@ -1,30 +1,36 @@
 var app = angular.module("app", ["fontService"]);
 
 app.directive("trypo", function() {
-  return { restrict: "E", scope: {}, templateUrl: "trypo.html" }
+  return { restrict: "E", scope: {}, transclude: true, templateUrl: "trypo.html" }
+});
+
+app.directive("controls", function() {
+  return { restrict: "E", scope: {}, templateUrl: "controls.html" }
 });
 
 app.controller("FontsController", function($scope, Font) {
   $scope.fonts = Font.all();
+  $scope.$on('receivedImages', function(event, fonts) {
+    console.log(fonts);
+    $scope.fonts = fonts;
+  });
 });
 
 app.controller("TrypoController", function($scope, $http, $filter, Feature) {
-  $scope.features = Feature.all();
-
+  var features = Feature.all();
   $scope.params = {
     text: "Brown fox jumping something something",
     size: "20",
     color: "#000000",
-    features: [] 
-  } 
-
-  $scope.changed = function() {
-    $http.post("/images", {
-      text: $scope.params.text,
-      size: $scope.params.size,
-      features: $filter('filter')($scope.features, { checked: true })
-    }).success(function(data, status, headers, config) {
-      console.log(data);
-    });
+    features: features 
   }
+
+  $scope.$watch("params", function(newValue) {
+    var data = angular.copy($scope.params);
+    data.features = $filter('filter')(data.features, { checked: true });
+    $http.post("/images", data).success(function(fonts) {
+      $scope.$emit('receivedImages', fonts);
+      // $scope.images = images 
+    });
+  }, true); 
 });
--- trypo.rb
+++ trypo.rb
@@ -1,7 +1,8 @@
-FONTS = Dir.glob('fonts/*').map do |file|
-  Tipo.info file
-end
+require 'base64'
 
+FONTS = Dir.glob('fonts/*').map do |file|
+  CheapRedWine.meta file 
+end
 
 class Trypo < Sinatra::Base
   get '/' do
@@ -11,20 +12,31 @@ class Trypo < Sinatra::Base
   get '/fonts' do
     content_type :json
     FONTS.map do |font|
-      { family: font.naming.family_name, style: font.naming.style }
+      { family: font.family, style: font.style, src: "" }
     end.to_json
   end
 
   get '/features' do
     content_type :json
-    FONTS.first.substitution.feature_list.map do |feature|
-      { tag: feature.tag }
+    FONTS.first.features.map do |feature|
+      { tag: feature } 
     end.to_json
   end
 
   post '/images' do
     content_type :json
     params = JSON.parse request.body.read
-    params.to_json
+    images = FONTS.map do |font|
+      features = params['features'].map { |f| f['tag'] }
+      image = CheapRedWine.image font,
+        params['text'],
+        size: params['size'].to_i,
+        color: params['color'],
+        features: features
+      data = { family: font.family, style: font.style, src: "data:image/png;base64,#{Base64.encode64(image.read)}" }
+      image.close
+      data
+    end
+    images.to_json
   end
 end
